231448fc8adf6c874c61b629314ae9ff3d38bad0
JONGGIL LEE <brianlee2k@naver.com>
2022-04-16 10:48:50 +0900

    Number, String, Variable(변수)

---
+++ syntax/String.js
@@ -0,0 +1,6 @@
+console.log(4 + 4);
+console.log("5" + "4");
+console.log(
+  "Apart from counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism"
+    .length
+);
