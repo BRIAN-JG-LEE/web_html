b9658ca52e5e568b119ff94d16b5b178af20173d
JONGGIL LEE <brianlee2k2@gmail.com>
2022-04-18 15:28:49 +0900

    제어문(조건문,선택문,반복문) 중 조건문 if 이용해 Not found page 구현

--- main.js
+++ main.js
@@ -4,39 +4,39 @@ var url = require("url");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  var queryData = new URL("http://localhost:3000" + _url).searchParams;
-  var title = queryData.get("id");
-  console.log(queryData.get("id"));
-  if (_url == "/") {
-    title = "Developer's";
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
+
+  if (pathname === "/") {
+    fs.readFile(`data/${queryData.id}`, "utf-8", (err, description) => {
+      var template = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>web-${title}</title>
+            <meta charset="utf-8" />
+          </head>
+          <body>
+            <h1><a href="/">Developer</a></h1>
+            <ul>
+              <li><a href="/?id=REACT">REACT</a></li>
+              <li><a href="/?id=SPRING">SPRING</a></li>
+              <li><a href="/?id=Node">Node.js</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>
+              ${description}
+            </p>
+          </body>
+        </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404);
+    response.end("Not found page");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.get("id")}`, "utf-8", (err, description) => {
-    var template = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>web-${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">Developer</a></h1>
-          <ul>
-            <li><a href="/?id=REACT">REACT</a></li>
-            <li><a href="/?id=SPRING">SPRING</a></li>
-            <li><a href="/?id=Node">Node.js</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>
-            ${description}
-          </p>
-        </body>
-      </html>
-    `;
-    response.end(template);
-  });
 });
 app.listen(3000);
