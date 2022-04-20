d93189ccdb6e14a0accbd50130b613088e723766
JONGGIL LEE <brianlee2k2@gmail.com>
2022-04-20 14:26:18 +0900

    데이터 파일 삭제(delete) - <input type="hidden"> 이용하기. *주의* method="post"

--- main.js
+++ main.js
@@ -69,7 +69,13 @@ var app = http.createServer(function (request, response) {
           <p>
             ${description}
           </p>`,
-            `<a href="/create">create cont</a> <a href="/update?id=${title}">update cont</a>`
+            `<a href="/create">create cont</a> 
+             <a href="/update?id=${title}">update cont</a>
+             <form action="/delete_process" method="post">
+              <input type="hidden" name="id" value="${title}" />
+              <input type="submit" value="delete" />
+             </form>
+             `
           );
           response.writeHead(200);
           response.end(template);
@@ -151,6 +157,21 @@ var app = http.createServer(function (request, response) {
           response.writeHead(302, { Location: `/?id=${title}` });
           response.end("success!");
         });
+      });
+    });
+  } else if (pathname === "/delete_process") {
+    var body = "";
+
+    request.on("data", function (data) {
+      body = body + data;
+    });
+
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (err) {
+        response.writeHead(302, { Location: `/` });
+        response.end("success!");
       });
     });
   } else {
