0336a80ad0954b5fea66e91d03eb2d8c3637f330
JONGGIL LEE <brianlee2k2@gmail.com>
2022-04-21 16:45:22 +0900

    .

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
+var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
-
-var template = require("./lib/template.js");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
